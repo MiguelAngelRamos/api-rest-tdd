c2dbe59a443fb7fcfede7ae1bfb2844bd7ebb79c
Miguel Angel Ramos <return.infox@gmail.com>
2023-12-21 17:37:27 -0300

    test terminados antes de generar el codigo del app y controlador

--- src/__tests__/integration/controllers/StudentController.test.ts
+++ src/__tests__/integration/controllers/StudentController.test.ts
@@ -1,6 +1,49 @@
 import request, {SuperTest, Test } from 'supertest';
 import { app, server } from '../../../../src/app';
 
+//* app.ts -> const server = app.listen(PORT, () => { console.log(`El server online: ${PORT}`)})
 describe('StudentController', () => {
+  let baseRequest: SuperTest<Test>;
+  //* Antes de cada una de las pruebas, configuramos la instancia de SuperTest
+  beforeAll(() => {
+    const address = server.address();
+    //* address tambien es un objeto
+    console.log(address);
+    if(!address || typeof address === 'string') {
+      throw new Error('Server is not running on an address with a port');
+    }
+    const port = address.port; //* Ahora que sabemos que address no es nulo y ademas que efectivamente es Objecto obtenemos el puerto.
+    baseRequest = request(`http://localhost:${port}`);
+  });
+
+  //* Después de todas las pruebas, cerramos el servidor
+  afterAll(done => {
+    server.close(done);
+  });
+
+  //* Prueba para el endpoint GET /students
+  describe('GET /students', () => {
+    it('should return a list of students', async () => {
+      const response = await baseRequest.get('/students');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeInstanceOf(Array); //* 10 students
+    });
+  });
+
+  //* Pruebas para el endpoint GET /students/:id
+  describe('GET /students/:id', () => {
+
+    it('should return a student by id', async () => {
+      const testId = 1;
+      const response = await baseRequest.get(`/students/${testId}`);
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('should return 404 for a non-existent student', async () => {
+      const testId = 99999;
+      const response = await baseRequest.get(`/students/${testId}`);
+      expect(response.statusCode).toBe(404);
+    });
+  });
 
 });
